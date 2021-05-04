13388c70ee2d07664c6034b9b3ab6aef3a98a42c
David <touisbetterthanpi@gmail.com>
2021-05-04 13:44:33 -0500

    Splits app into a menu and a Fighter container, + `add_fighter_bttn`

--- src/main.c
+++ src/main.c
@@ -4,6 +4,30 @@
 #include "../include/utilities/lowlevel.h"
 
 
+void gobbo_add_fighter_callback(GtkWidget* fighter_container) {
+    GobboFighter* fighter = gobbo_fighter_new("Goblin");
+    gobbo_fighter_add_stat(fighter, "Intelligence");
+    gobbo_fighter_add_stat(fighter, "Wisdom");
+    gobbo_fighter_add_stat(fighter, "Charisma");
+    gobbo_fighter_add_stat(fighter, "Dexterity");
+    gobbo_fighter_add_stat(fighter, "Strength");
+    gobbo_fighter_reformat(fighter);
+
+    gtk_container_add(GTK_CONTAINER(fighter_container), GTK_WIDGET(fighter));
+    gtk_widget_show_all(fighter_container);
+}
+
+GtkWidget* gobbo_create_menu(GtkContainer* fighter_container) {
+    GtkWidget* menu_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+
+    GtkWidget* add_fighter_bttn = gtk_button_new_with_label("Add Fighter");
+    g_signal_connect_swapped(add_fighter_bttn, "clicked", G_CALLBACK(gobbo_add_fighter_callback), fighter_container);
+
+    gtk_container_add(GTK_CONTAINER(menu_container), add_fighter_bttn);
+
+    return menu_container;
+}
+
 static void
 activate (GtkApplication* app,
           gpointer        user_data)
@@ -14,21 +38,14 @@ activate (GtkApplication* app,
     gtk_window_set_title (GTK_WINDOW (window), "Window");
     gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
-    gobbo_validate_fighter();
+    GtkWidget* fighter_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    GtkWidget* menu_container = gobbo_create_menu(GTK_CONTAINER(fighter_container));
 
-    GobboFighter* fighter = gobbo_fighter_new("Goblin");
-    gobbo_fighter_add_stat(fighter, "Intelligence");
-    gobbo_fighter_add_stat(fighter, "Wisdom");
-    gobbo_fighter_add_stat(fighter, "Charisma");
-    gobbo_fighter_add_stat(fighter, "Dexterity");
-    gobbo_fighter_add_stat(fighter, "Strength");
-    gobbo_fighter_reformat(fighter);
-    //just here to prove that repeated calls to `gobbo_fighter_reformat`
-    //are handled correctly. remove next time this file is modified
-    gobbo_fighter_reformat(fighter);
-    gobbo_fighter_reformat(fighter);
-    
-    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(fighter));
+    GtkWidget* main_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+    gtk_container_add(GTK_CONTAINER(main_box), menu_container);
+    gtk_container_add(GTK_CONTAINER(main_box), fighter_container);
+
+    gtk_container_add(GTK_CONTAINER(window), main_box);
 
     gtk_widget_show_all (window);
 
